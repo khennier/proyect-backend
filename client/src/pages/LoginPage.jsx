c76da3ee300206b18a73afa3dab72562e7f8212d
khennier <78939009+khennier@users.noreply.github.com>
2024-03-14 17:27:48 -0300

    Actualizando Login Page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,6 +1,58 @@
 function LoginPage() {
     return (
-        <div>LoginPage</div>
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label>
+                    <input
+                        type="text"
+                        name="username"
+                        {...register("username", { required: true })}
+                        className="bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+                        placeholder='Username'
+                    />
+                        {
+                            errors.username && (
+                                <p className="text-red-500">
+                                    Username is required
+                                </p>
+                            )
+                        }
+                </label>
+                <label>
+                    <input
+                        type="email"
+                        name="email"
+                        {...register("email", { required: true })}
+                        className="bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+                        placeholder='Email'
+                    />
+                    {
+                            errors.email && (
+                                <p className="text-red-500">
+                                    Email is required
+                                </p>
+                            )
+                    }
+                </label>
+                <label>
+                    <input
+                        type="password"
+                        name="password"
+                        {...register("password", { required: true })}
+                        className="bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+                        placeholder='Password'
+                    />
+                    {
+                            errors.password && (
+                                <p className="text-red-500">
+                                    Password is required
+                                </p>
+                            )
+                    }
+                </label>
+                <button type="submit">Register</button>
+            </form>
+        </div>
     )
     
 }
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,7 +6,6 @@ import {createAccessToken} from '../libs/jwt.js'
 export const register = async (req,res) => {
     const {email, password, username} = req.body;
 
-
     try {
         const userFound = await User.findOne({email})
         if (userFound) return res.status(400).json(["The email is already in use"])
