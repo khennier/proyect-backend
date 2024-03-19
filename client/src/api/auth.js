47369d831ddfcccfb5d5bd90e4e8c4596c253260
khennier <78939009+khennier@users.noreply.github.com>
2024-03-19 19:21:08 -0300

    actualizando error

--- client/src/api/auth.js
+++
@@ -1,7 +0,0 @@
-import axios from './axios.js'
-
-export const registerRequest = user => axios.post(`/register`, user)
-
-export const loginRequest = user => axios.post(`/login`, user)
-
-export const verifyTokenRequest = () => axios.get('/verify')
--- src/config.js
+++
@@ -1,3 +0,0 @@
-const TOKEN_SECRET = 'some secret key';
-
-export { TOKEN_SECRET };
