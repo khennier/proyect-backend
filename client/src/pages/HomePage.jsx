56ab50932c83fdac90d7d2ae0f764ef00223ae03
khennier <78939009+khennier@users.noreply.github.com>
2024-03-24 12:42:31 -0300

    finalizando

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,16 @@
+import { Link } from 'react-router-dom'
 function HomePage() {
-    return (
-        <div>HomePage</div>
-    )
-    
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold mb-8">Welcome to Your Task Manager</h1>
+      <Link
+        to="/tasks"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md transition duration-300 ease-in-out"
+      >
+        Go to Tasks
+      </Link>
+    </div>
+  );
 }
-export default HomePage
+
+export default HomePage;
--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,7 +1,30 @@
+import { useAuth } from "../context/AuthContext";
+
 function ProfilePage() {
+  const { isAuthenticated, user } = useAuth();
   return (
-    <div>ProfilePage</div>
-  )
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold mb-8">User Profile</h1>
+      <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
+        <div className="mb-4">
+          <label htmlFor="name" className="block text-sm font-medium text-white">
+            { isAuthenticated && (
+              <h1 className="text-lg text-gray-200"> Name: {user.username}</h1>
+            )
+            }
+          </label>
+        </div>
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-sm font-medium text-white">
+          { isAuthenticated && (
+              <h1 className="text-lg text-gray-200"> Email: {user.email}</h1>
+            )
+            }
+          </label>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ProfilePage
+export default ProfilePage;
